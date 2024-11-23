391063a8f0f85ed50429791e22a9422da229e6ad
Yuri Galvao <yuri.gvao@gmail.com>
2024-11-23 17:14:49 -0300

    created ProductList

--- src/app/(products)/components/ProductItem.tsx
+++ src/app/(products)/components/ProductItem.tsx
@@ -1,23 +1,38 @@
+'use client'
+
 import Button from "@/app/components/Button"
 import Image from "next/image"
 import { Subtitle } from "@/app/components/Title"
-import { IProduct } from "@/Domain/Product/IProduct"
+import { IProductDto } from "@/Domain/Product/interface"
+import Fetcher from "@/utils/Fetcher"
+import ShopCartGateway from "@/infra/gateway/ShopCartGateway"
 
-const ProductItem = ({ product, ...props }: { product: IProduct }) =>
-    <li {...props} className="bg-slate-900 flex flex-col gap-2 basis-[48%] sm:basis-[32%]" >
+const ProductItem = ({ product, ...props }: { product: IProductDto }) => {
 
-        <div className={`w-full bg-red-600`}>
-            <Image src={product.src} alt={`image ${product.name}`} width={400} height={400} className="object-right" />
-        </div>
+    const fetcher = new Fetcher(process.env.URL_BASE)
+    const shopCartGateway = new ShopCartGateway(fetcher)
+    
+    const addQuantityInShopCartItem = (product: IProductDto) => {
+        shopCartGateway.addShopCartItem({ quantity: 1, product })
+    }
 
-        <div className="p-2">
-            <Subtitle>{product.name}</Subtitle>
-            <p className="px-2">R$ {product.price.toFixed(2)}</p>
-        </div>
+    return (
+        <li {...props} className="bg-slate-900 flex flex-col gap-2 basis-[48%] sm:basis-[32%]" >
 
-        <div className="flex justify-center py-4" >
-            <Button>Adicionar</Button>
-        </div>
-    </li>
+            <div className={`w-full`}>
+                <Image src={product.src} alt={`image ${product.name}`} width={400} height={400} className="object-right" />
+            </div>
+
+            <div className="p-2">
+                <Subtitle>{product.name}</Subtitle>
+                <p className="px-2">R$ {product.price.toFixed(2)}</p>
+            </div>
+
+            <div className="flex justify-center py-4" >
+                <Button onClick={() => addQuantityInShopCartItem(product)}>Adicionar</Button>
+            </div>
+        </li>
+    )
+}
 
 export default ProductItem
--- src/app/(products)/components/ProductList.tsx
+++ src/app/(products)/components/ProductList.tsx
@@ -1,7 +1,16 @@
 import ProductItem from "./ProductItem"
-import { products } from "../../../Domain/Product"
+import Fetcher from "@/utils/Fetcher"
+import ProductsGateway from "@/infra/gateway/ProductsGateway"
 
-export default function ProductList() {
+export default async function ProductList() {
+    const fetcher = new Fetcher(process.env.URL_BASE)
+    const productsGateway = new ProductsGateway(fetcher)
+    const products = await productsGateway.getAllProducts()
+
+    if (products.length === 0) {
+        return <p className="text-center">Nenhum produto encontrado.</p>
+    }
+
     return <>
         <ul className="flex gap-3 justify-between items-center p-4 px-8 flex-wrap" >
             {products.map(product => (
