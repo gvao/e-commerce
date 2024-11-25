d3609d299d94294d963b9fd1e307825ac47777c1
Yuri Galvao <yuri.gvao@gmail.com>
2024-11-24 21:02:51 -0300

    created DecrementQuantityShopCartItemById use case

---
+++ src/application/UseCases/DecreaseQuantityShopCartItemById.ts
@@ -0,0 +1,22 @@
+import IShopCartItem from "@/Domain/ShopCartItem/interface"
+import { GetById, Save } from "../repository"
+import UseCase from "./interface"
+
+export default class DecreaseQuantityShopCartItemById implements UseCase<Input, Output> {
+    constructor(private repository: repository) { }
+
+    async execute({ id, quantity }: Input): Promise<void> {
+        const shopCartItem = await this.repository.getById(id)
+        if(!shopCartItem) throw new Error(`shopCartItem not found`)
+        shopCartItem?.decreaseQuantity(quantity)
+        await this.repository.save(shopCartItem)
+    }
+}
+
+type repository = GetById<IShopCartItem> & Save<IShopCartItem>
+
+type Input = {
+    id: string,
+    quantity: number,
+}
+type Output = void
---
+++ src/tests/application/DecreaseQuantityShopCartItemById.test.ts
@@ -0,0 +1,26 @@
+import { beforeAll, describe, expect, it } from "vitest";
+
+import Product from "../../Domain/Product";
+import ShopCartItem from "../../Domain/ShopCartItem";
+import ShopCartItemRepository from "../../infra/repositories/ShopCartItemRepository";
+import DecreaseQuantityShopCartItemById from "../../application/UseCases/DecreaseQuantityShopCartItemById";
+
+describe("DecreaseQuantityShopCartItemById", () => {
+    const shopCartItemRepository = new ShopCartItemRepository()
+    const decreaseQuantityShopCartItemById = new DecreaseQuantityShopCartItemById(shopCartItemRepository)
+    let fakeId = ''
+    beforeAll(async () => {
+        const quantity = 2
+        const product = Product.create({ name: 'any_name', price: 100, src: 'any_src' })
+        const shopCartItem = ShopCartItem.create({ product: product.dto, quantity })
+        fakeId = shopCartItem.id
+        await shopCartItemRepository.save(shopCartItem)
+    })
+
+    it('decrease quantity shopCartItem', async () => {
+        await decreaseQuantityShopCartItemById.execute({ id: fakeId, quantity: 1 })
+        const shopCartItem = await shopCartItemRepository.getById(fakeId)
+        expect(shopCartItem?.quantity).toBe(1)
+
+    })
+})
